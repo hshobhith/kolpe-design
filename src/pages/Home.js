8626fe9ba00e04b9c77f07d6852a042419eef2dd
hshobhith <hshobhith@gmail.com>
2025-06-27 11:26:06 +0530

    Updated home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,9 @@
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; // Import the carousel CSS
-import slide1 from '../images/slide1.png';
-import slide2 from '../images/slide2.png';
-import slide3 from '../images/slide3.png';
+import slide1 from '../images/projects/InShot_20231121_201013879,jpg';
+import slide2 from '../images/projects/InShot_20240822_141306026.jpg';
+import slide3 from '../images/projects/InShot_20240510_165912582.jpg';
+import slide4 from '../images/projects/InShot_20240920_180558474.jpg';
 
 export default function Home() {
   return (
@@ -20,13 +21,16 @@ export default function Home() {
             swipeable
           >
             <div>
+              <img src={slide3} alt="Slide 3" className="rounded-lg" />
+            </div>
+            <div>
+              <img src={slide4} alt="Slide 4" className="rounded-lg" />
+            </div>
+            <div>
               <img src={slide1} alt="Slide 1" className="rounded-lg" />
             </div>
             <div>
               <img src={slide2} alt="Slide 2" className="rounded-lg" />
-            </div>
-            <div>
-              <img src={slide3} alt="Slide 3" className="rounded-lg" />
             </div>
           </Carousel>
         </div>
